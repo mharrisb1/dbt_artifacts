0eb16de64aa88c0336034e0fd18acd809b34eee0
Michael Harris <mharris@luabase.com>
2022-10-13 17:39:49 -0400

    Use case-sensitive hash function for clickhouse adapter

--- macros/surrogate_key.sql
+++ macros/surrogate_key.sql
@@ -54,3 +54,50 @@ multiple string arguments. The {}.{} model triggered this warning. \
 {{ hash(concat(fields)) }}
 
 {%- endmacro -%}
+
+
+{%- macro clickhouse__surrogate_key(field_list) -%}
+
+{%- if varargs|length >= 1 or field_list is string %}
+
+{%- set error_message = '
+Warning: the `surrogate_key` macro now takes a single list argument instead of \
+multiple string arguments. The {}.{} model triggered this warning. \
+'.format(model.package_name, model.name) -%}
+
+{%- do exceptions.warn(error_message) -%}
+
+{# first argument is not included in varargs, so add first element to field_list_xf #}
+{%- set field_list_xf = [field_list] -%}
+
+{%- for field in varargs %}
+{%- set _ = field_list_xf.append(field) -%}
+{%- endfor -%}
+
+{%- else -%}
+
+{# if using list, just set field_list_xf as field_list #}
+{%- set field_list_xf = field_list -%}
+
+{%- endif -%}
+
+
+{%- set fields = [] -%}
+
+{%- for field in field_list_xf -%}
+
+    {%- set _ = fields.append(
+        "coalesce(cast(" ~ field ~ " as " ~ type_string() ~ "), '')"
+    ) -%}
+
+    {%- if not loop.last %}
+        {%- set _ = fields.append("'-'") -%}
+    {%- endif -%}
+
+{%- endfor -%}
+
+{#- Clickhouse is case-sensitive so we have to use `MD5` instead of `md5` -#}
+
+MD5({{ concat(fields) }})
+
+{%- endmacro -%}
