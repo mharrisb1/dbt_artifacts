688a0659f7007c8639bdb1a9a3d71a3c3bb60978
Michael Harris <mharris@luabase.com>
2022-10-13 17:54:07 -0400

    Variables should be templated

--- macros/split_part.sql
+++ macros/split_part.sql
@@ -3,9 +3,9 @@
 {%- endmacro %}
 
 {% macro default__split_part(string_text, delimiter_text, part_number) -%}
-    {{ dbt.split_part(string_text, delimiter_text, part_number) }}
+    {{ dbt.split_part({{ string_text }}, {{ delimiter_text }}, {{ part_number }}) }}
 {%- endmacro %}
 
 {% macro clickhouse__split_part(string_text, delimiter_text, part_number) -%}
-    splitByString(delimiter_text, string_text)[part_number]
+    splitByString({{ delimiter_text }}, {{ string_text }})[{{ part_number }}]
 {%- endmacro %}
