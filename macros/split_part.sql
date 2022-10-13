8b21d0a32734955685bfd41b0e827055b053e756
Michael Harris <mharris@luabase.com>
2022-10-13 17:52:15 -0400

    Add proxy interface for `split_part` to handle Clickhouse adapter

---
+++ macros/split_part.sql
@@ -0,0 +1,11 @@
+{% macro split_part(string_text, delimiter_text, part_number) -%}
+    {{ return(adapter.dispatch('split_part')(string_text, delimiter_text, part_number)) }}
+{%- endmacro %}
+
+{% macro default__split_part(string_text, delimiter_text, part_number) -%}
+    {{ dbt.split_part(string_text, delimiter_text, part_number) }}
+{%- endmacro %}
+
+{% macro clickhouse__split_part(string_text, delimiter_text, part_number) -%}
+    splitByString(delimiter_text, string_text)[part_number]
+{%- endmacro %}
