221a7eb9001469d52b921c363c5c9161029b6d96
Michael Harris <mharris@luabase.com>
2022-10-13 17:23:29 -0400

    Fix adapter-specific macros for `cast_null_as_type`

--- macros/cast_null_as_type.sql
+++ macros/cast_null_as_type.sql
@@ -2,10 +2,10 @@
     {{ return(adapter.dispatch('cast_null_as_type')(type)) }}
 {%- endmacro %}
 
-{% macro default__cast_null_as_type() -%}
+{% macro default__cast_null_as_type(type) -%}
     cast(null as {{ type }})
 {%- endmacro %}
 
-{% macro clickhouse__cast_null_as_type() %}
+{% macro clickhouse__cast_null_as_type(type) %}
     cast(null, 'Nullable({{ type }})')
 {% endmacro %}
