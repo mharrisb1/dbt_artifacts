128d40183cd2602935d92ba20a8c08486c7f91e0
Michael Harris <mharris@luabase.com>
2022-10-14 09:36:27 -0400

    Add Clickhouse profile

--- integration_test_project/example-env.sh
+++ integration_test_project/example-env.sh
@@ -16,6 +16,9 @@ export DBT_ENV_SECRET_DATABRICKS_TOKEN=
 export DBT_ENV_SECRET_GCP_PROJECT=
 export DBT_ENV_SPARK_DRIVER_PATH= # /Library/simba/spark/lib/libsparkodbc_sbu.dylib on a Mac
 export DBT_ENV_SPARK_ENDPOINT= # The endpoint ID from the Databricks HTTP path
+export DBT_ENV_SECRET_CLICKHOUSE_USER=
+export DBT_ENV_SECRET_CLICKHOUSE_PASSWORD=
+export DBT_ENV_SECRET_CLICKHOUSE_HOST=
 
 # dbt environment variables, change these
 export DBT_CLOUD_PROJECT_ID=
